d2f88b5e3581e7e28aa5cc948dc5cc395c9f8e5b
Henrique Ventura <trycode2018@gmail.com>
2023-09-13 23:55:27 +0100

    Terminando o crud adicionando os metodos update e delete

--- posts/urls.py
+++ posts/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('homepage',views.homepage,name='posts_home'),
     path('',views.list_posts,name='list_posts'),
     path('<int:post_id>',views.post_detail,name='post_detail'),
+    path('update/<int:post_id>/',views.update_post,name='update_post'),
+    path('delete/<int:post_id>/',views.delete_post,name='delete_post')
 ]
 
 
--- posts/views.py
+++ posts/views.py
@@ -52,3 +52,32 @@ def post_detail(request:Request,post_id:int):
         'data':serializer.data
     }
     return Response(data=response,status=status.HTTP_200_OK)
+
+@api_view(http_method_names=['PUT'])
+def update_post(request:Request,post_id:int):
+    post = get_object_or_404(Post,pk=post_id)
+    
+    data = request.data
+    serializer = PostSerializer(instance=post,data=data)
+    
+    if serializer.is_valid():
+        serializer.save()
+        
+        response = {
+            'message':'Updated post successfuly',
+            'data':serializer.data
+        }
+        
+        return Response(data=response,status=status.HTTP_200_OK)
+    return Response(data=serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(http_method_names=['DELETE'])
+def delete_post(request:Request,post_id:int):
+    post = get_object_or_404(Post,pk=post_id)
+    
+    post.delete()
+    
+    return Response(status=status.HTTP_204_NO_CONTENT)
+        
+        
+        
